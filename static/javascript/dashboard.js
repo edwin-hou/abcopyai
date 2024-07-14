9327e2673b080be3d02038cf1e8f45af64dac464
EdwinHou <43428344+edwin-hou@users.noreply.github.com>
2024-07-14 03:32:34 -0700

    Update dashboard.js

--- static/javascript/dashboard.js
+++ static/javascript/dashboard.js
@@ -1,9 +1,9 @@
 var socket = io.connect('http://54.172.23.104/')
 //console.log(socket)
 socket.on('connect', function () {
-console.log('a')
 socket.emit("get_data", "edwin.s.hou@gmail.com")
 socket.on('data', (emails)=>{
+    console.log(emails)
     for(let i =0; i< emails.length; i++){
     let email = document.createElement("p");
     email.textContent = JSON.stringify(emails[i])
