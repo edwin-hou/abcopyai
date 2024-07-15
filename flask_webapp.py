30fc6526e68618a0bed0a7154cfb72409be32e5d
EdwinHou <43428344+edwin-hou@users.noreply.github.com>
2024-07-15 13:26:23 -0700

    Update flask_webapp.py

--- flask_webapp.py
+++ flask_webapp.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # CORS(app)
 
 socketio = SocketIO(app)
-socketio.init_app(app, cors_allowed_origins="*")
+socketio.init_app(app, cors_allowed_origins="*" )
 
 @app.route('/mailer')
 def mailer():
