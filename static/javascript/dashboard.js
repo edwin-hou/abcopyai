d3a0e6a3bd270cb8f869812cbdcfbe178c38572c
EdwinHou <43428344+edwin-hou@users.noreply.github.com>
2024-07-14 23:13:26 -0700

    Update dashboard.js

--- static/javascript/dashboard.js
+++ static/javascript/dashboard.js
@@ -1,10 +1,10 @@
 var socket = io('http://54.172.23.104/')
-console.log(socket, new Date().toLocaleString())
+console.log(new Date().toLocaleString(), socket)
 socket.on('connect', function () {
-console.log("connected", new Date().toLocaleString())
+console.log(new Date().toLocaleString(), "connected")
 socket.emit("get_data", "edwin.s.hou@gmail.com")
 socket.on('data', (emails)=>{
-    console.log(emails, new Date().toLocaleString())
+    console.log(new Date().toLocaleString(), emails)
     for(let i =0; i< emails.length; i++){
     let email = document.createElement("p");
     email.textContent = JSON.stringify(emails[i])
