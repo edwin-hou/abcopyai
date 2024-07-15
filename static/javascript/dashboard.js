220f427b34437411425f13f3f4a2244daca41fa6
EdwinHou <43428344+edwin-hou@users.noreply.github.com>
2024-07-14 23:41:31 -0700

    Update dashboard.js

--- static/javascript/dashboard.js
+++ static/javascript/dashboard.js
@@ -1,16 +1,16 @@
-var socket = io.connect('http://54.172.23.104/')
+var socket = io.connect('http://54.172.23.104')
 console.log(new Date().toLocaleString(), socket)
 socket.on('connect', function () {
-console.log(new Date().toLocaleString(), "connected")
-socket.emit("get_data", "edwin.s.hou@gmail.com")
-console.log(new Date().toLocaleString(), "emitted")
-socket.on('data', (emails)=>{
-    console.log(new Date().toLocaleString(), emails)
-    for(let i =0; i< emails.length; i++){
-    let email = document.createElement("p");
-    email.textContent = JSON.stringify(emails[i])
-    document.querySelector("div#data").appendChild(email)
-    }
+    console.log(new Date().toLocaleString(), "connected")
+    socket.emit("get_data", "edwin.s.hou@gmail.com")
+    console.log(new Date().toLocaleString(), "emitted")
+    socket.on('data', (emails) => {
+        console.log(new Date().toLocaleString(), emails)
+        for (let i = 0; i < emails.length; i++) {
+            let email = document.createElement("p");
+            email.textContent = JSON.stringify(emails[i])
+            document.querySelector("div#data").appendChild(email)
+        }
 
-})
+    })
 })
