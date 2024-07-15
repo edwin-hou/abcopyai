bf1e99983c7fb58fa5fe7d97e7d54baa2967b1ba
EdwinHou <43428344+edwin-hou@users.noreply.github.com>
2024-07-15 03:13:02 -0700

    Update dashboard.js

--- static/javascript/dashboard.js
+++ static/javascript/dashboard.js
@@ -1,11 +1,11 @@
 var socket = io.connect({'force new connection': true, transports: ['WebSocket', 'Flash Socket', 'AJAX long-polling']})
-//console.log(new Date().toLocaleString(), socket)
+console.log(new Date().toLocaleString(), socket)
 socket.on('connect', function () {
-//    console.log(new Date().toLocaleString(), "connected")
+    console.log(new Date().toLocaleString(), "connected")
     socket.emit("get_data", "edwin.s.hou@gmail.com")
-//    console.log(new Date().toLocaleString(), "emitted")
+    console.log(new Date().toLocaleString(), "emitted")
     socket.on('data', (emails) => {
-//        console.log(new Date().toLocaleString(), emails)
+        console.log(new Date().toLocaleString(), emails)
         for (let i = 0; i < emails.length; i++) {
             let email = document.createElement("p");
             email.textContent = JSON.stringify(emails[i])
