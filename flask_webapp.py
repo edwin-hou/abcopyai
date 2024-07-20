e55516d75f6f00311e12d7e57f66fb0280316eb5
EdwinHou <43428344+edwin-hou@users.noreply.github.com>
2024-07-20 15:18:46 -0700

    Update flask_webapp.py

--- flask_webapp.py
+++ flask_webapp.py
@@ -1,70 +1,52 @@
-from flask_socketio import SocketIO, emit
-from flask import Flask, render_template, jsonify, request, send_file
-from send_email import send_email
-from authenticate import authenticate
-from copy_ai import generate_copies
-import json
-import google_auth_oauthlib
-import flask
 import os
-import pickle
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
-app = Flask(__name__)
-app.secret_key = 'superfi secret key'
+import flask
 
-socketio = SocketIO(app)
-socketio.init_app(app, cors_allowed_origins="*")
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
 
+# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
+# the OAuth 2.0 information for this application, including its client_id and
+# client_secret.
+CLIENT_SECRETS_FILE = "credentials.json"
 
-@app.route('/mailer')
-def mailer():
-    return render_template('mailer.html')
+# This OAuth 2.0 access scope allows for full read/write access to the
+# authenticated user's account and requires requests to use an SSL connection.
+SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+API_SERVICE_NAME = 'youtube'
+API_VERSION = 'v3'
 
-
-@app.route('/dashboard')
-def dashboard():
-    print('dashboard loaded')
-    return render_template('dashboard.html')
+app = flask.Flask(__name__)
+# Note: A secret key is included in the sample so that it works, but if you
+# use this code in your application please replace this with a truly secret
+# key. See http://flask.pocoo.org/docs/0.12/quickstart/#sessions.
+app.secret_key = 'REPLACE ME - this value is here as a placeholder.'
 
 
 @app.route('/')
-def home():
-    return render_template('landing.html')
+def index():
+    if 'credentials' not in flask.session:
+        return flask.redirect('authorize')
 
+    # Load the credentials from the session.
+    credentials = google.oauth2.credentials.Credentials(
+        **flask.session['credentials'])
 
-@app.route('/email_open', methods=['POST', 'GET'])
-def email_open():
-    data = eval(list(request.form)[0])
-    print(eval(list(request.form)[0])['ip'])
-    return "aa"
+    client = googleapiclient.discovery.build(
+        API_SERVICE_NAME, API_VERSION, credentials=credentials)
 
+    return channels_list_by_username(client,
+                                     part='snippet,contentDetails,statistics',
+                                     forUsername='GoogleDevelopers')
 
-@app.route('/image/<path:id>')
-def spy_pixel(id):
-    with open('static/database.json', 'r+') as f:
-        data = json.load(f)
-        for user in data.keys():
-            for email in data[user]:
-                if email["id"] == int(id):
-                    email["opened"] = True
-
-        f.seek(0)  # <--- should reset file position to the beginning.
-        json.dump(data, f, indent=4)
-        f.truncate()  # remove remaining part
-
-    return send_file("static/spy.gif", mimetype="image/png")
-
-SCOPES = ['https://www.googleapis.com/auth/gmail.send',
-              'https://www.googleapis.com/auth/gmail.modify']
 
 @app.route('/authorize')
 def authorize():
     # Create a flow instance to manage the OAuth 2.0 Authorization Grant Flow
     # steps.
-
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-        "credentials.json", scopes=SCOPES)
+        CLIENT_SECRETS_FILE, scopes=SCOPES)
     flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
     authorization_url, state = flow.authorization_url(
         # This parameter enables offline access which gives your application
@@ -82,12 +64,11 @@ def authorize():
 
 @app.route('/oauth2callback')
 def oauth2callback():
-    home_dir = os.path.expanduser('~')
     # Specify the state when creating the flow in the callback so that it can
     # verify the authorization server response.
     state = flask.session['state']
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-        "credentials.json", scopes=SCOPES, state=state)
+        CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
     flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
 
     # Use the authorization server's response to fetch the OAuth 2.0 tokens.
@@ -99,63 +80,27 @@ def oauth2callback():
     #     Store user's access and refresh tokens in your data store if
     #     incorporating this code into your real app.
     credentials = flow.credentials
-    pickle_path = os.path.join(home_dir, 'gmail.pickle')
-    print(credentials)
-    print('==================')
-    with open(pickle_path, 'wb') as token:
-        pickle.dump(credentials, token)
-    # flask.session['credentials'] = {
-    #     'token': credentials.token,
-    #     'refresh_token': credentials.refresh_token,
-    #     'token_uri': credentials.token_uri,
-    #     'client_id': credentials.client_id,
-    #     'client_secret': credentials.client_secret,
-    #     'scopes': credentials.scopes
-    # }
+    flask.session['credentials'] = {
+        'token': credentials.token,
+        'refresh_token': credentials.refresh_token,
+        'token_uri': credentials.token_uri,
+        'client_id': credentials.client_id,
+        'client_secret': credentials.client_secret,
+        'scopes': credentials.scopes
+    }
 
-    return flask.redirect(flask.url_for('home'))
+    return flask.redirect(flask.url_for('index'))
 
 
-# @socketio.on('login')
-# def handle():
-#     auth = authenticate()
-#     auth_url = next(auth)
-#     print('aaaaa')
-#     emit("login_url", auth_url)
-#     # print(next(auth))
-#     # response = ""
-#     # response = jsonify({'source_url': source_url, "author": author, "source": source, "date": date, "credibility": credibility})
-#     # response.headers.add('Access-Control-Allow-Origin', '*')
-#     # print(source_url)
+def channels_list_by_username(client, **kwargs):
+    response = client.channels().list(
+        **kwargs
+    ).execute()
 
-
-@socketio.on('get_data')
-def handle(user):
-    print('getting data')
-    # emit("data", "aaaa")
-    with open('static/database.json', 'r') as f:
-        data = json.load(f)
-        emit("data", data[user])
-
-
-@socketio.on('mail')
-def handle(stuff):
-    # copies = generate_copies(stuff['subject'])
-    copies = [stuff['subject']]
-    copies.append(stuff['subject'])
-    print(copies)
-    print(stuff["url"])
-    mailing_list = stuff['mailing_list'].split(',')
-    i = 0
-    for email in mailing_list:
-
-        send_email("edwin.s.hou@gmail.com", email, copies[i % 4], stuff['content'], stuff['url'])
-        print("LETSGOOO we just emailed: " + email)
-        i += 1
-
-
+    return flask.jsonify(**response)
 if __name__ == "__main__":
     home_dir = os.path.expanduser('~')
     pickle_path = os.path.join(home_dir, 'gmail.pickle')
     print(pickle_path)
-    socketio.run(app, port=8000, debug=True, log_output=True, allow_unsafe_werkzeug=True)
+    app.run(port=8000, debug=True, log_output=True, allow_unsafe_werkzeug=True)
+    # socketio.run(app, port=8000, debug=True, log_output=True, allow_unsafe_werkzeug=True)
