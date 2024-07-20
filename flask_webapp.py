669094c6bb2af9780410c5455a394b13ae635e23
EdwinHou <43428344+edwin-hou@users.noreply.github.com>
2024-07-20 14:23:14 -0700

    Update flask_webapp.py

--- flask_webapp.py
+++ flask_webapp.py
@@ -4,13 +4,18 @@ from send_email import send_email
 from authenticate import authenticate
 from copy_ai import generate_copies
 import json
-from flask_cors import CORS
+import google_auth_oauthlib
+import flask
+import os
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 app = Flask(__name__)
-# CORS(app)
+app.secret_key = 'superfi secret key'
 
 socketio = SocketIO(app)
 socketio.init_app(app, cors_allowed_origins="*")
+
 
 @app.route('/mailer')
 def mailer():
@@ -50,6 +55,60 @@ def spy_pixel(id):
 
     return send_file("static/spy.gif", mimetype="image/png")
 
+SCOPES = ['https://www.googleapis.com/auth/gmail.send',
+              'https://www.googleapis.com/auth/gmail.modify']
+
+@app.route('/authorize')
+def authorize():
+    # Create a flow instance to manage the OAuth 2.0 Authorization Grant Flow
+    # steps.
+
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        "credentials.json", scopes=SCOPES)
+    flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
+    authorization_url, state = flow.authorization_url(
+        # This parameter enables offline access which gives your application
+        # both an access and refresh token.
+        access_type='offline',
+        # This parameter enables incremental auth.
+        include_granted_scopes='true')
+
+    # Store the state in the session so that the callback can verify that
+    # the authorization server response.
+    flask.session['state'] = state
+
+    return flask.redirect(authorization_url)
+
+
+@app.route('/oauth2callback')
+def oauth2callback():
+    # Specify the state when creating the flow in the callback so that it can
+    # verify the authorization server response.
+    state = flask.session['state']
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        "credentials.json", scopes=SCOPES, state=state)
+    flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
+
+    # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+    authorization_response = flask.request.url
+    flow.fetch_token(authorization_response=authorization_response)
+
+    # Store the credentials in the session.
+    # ACTION ITEM for developers:
+    #     Store user's access and refresh tokens in your data store if
+    #     incorporating this code into your real app.
+    credentials = flow.credentials
+    flask.session['credentials'] = {
+        'token': credentials.token,
+        'refresh_token': credentials.refresh_token,
+        'token_uri': credentials.token_uri,
+        'client_id': credentials.client_id,
+        'client_secret': credentials.client_secret,
+        'scopes': credentials.scopes
+    }
+
+    return flask.redirect("/")
+
 
 @socketio.on('login')
 def handle():
@@ -87,4 +146,4 @@ def handle(stuff):
 
 
 if __name__ == "__main__":
-    socketio.run(app, port = 8000, debug=True, log_output=True)
+    socketio.run(app, port=8000, debug=True, log_output=True, allow_unsafe_werkzeug=True)
